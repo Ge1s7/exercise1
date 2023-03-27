95dc3bddfcf057f127c9b809c2a6eb82b0fb8400
Marco Palma <operatr@MacBook-Pro.local>
2023-03-26 22:10:59 -0500

    convert hex string to array of 24bit structs

--- base64/base64.c
+++ base64/base64.c
@@ -1,10 +1,11 @@
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 #include <stdio.h>
 #include "base64.h"
 
 typedef struct encoding_gadget {
-	unsigned char octet1, octet2, octet3;
+	unsigned char octet0, octet1, octet2;
 }encoding_gadget;
 
 const char BASE64_ALPHABET[] = {
@@ -18,22 +19,52 @@ const char BASE64_ALPHABET[] = {
     '4', '5', '6', '7', '8', '9', '+', '/'
 };
 
-const char *hex_to_base64(const char *hex_str)
+int gadget_arr_len(size_t hex_str_len)
 {
-	printf("%s\n", hex_str);
-	const int hex_str_len = strlen(hex_str);
+	int ret_val = 0;
 
-	for (int i = 0; i < hex_str_len; i += 3) {
-		if (hex_str_len < 3) {
-			
-		}
-
-		encoding_gadget gadget = {
-			hex_str[i],
-			hex_str[i + 1],
-			hex_str[i + 2]
-		};
-		printf("octet1: %c, octet2: %c, octet3: %c\n", gadget.octet1, gadget.octet2, gadget.octet3);
+	if (hex_str_len <= 3) {
+		ret_val = 1;
 	}
-	return NULL;
+	else {
+		ret_val = (int)ceil(hex_str_len / 3);
+	}
+
+	return ret_val;
+}
+
+encoding_gadget *build_gadget_array(const char *hex_str, int hex_str_len, int gadget_array_len)
+{
+	encoding_gadget *gadgets = malloc(sizeof(encoding_gadget) * gadget_array_len);
+	int idx = 0;
+	int octet_index = 0;
+
+	for (int i = 0; i < hex_str_len; i++) {
+		if (octet_index == 0) {
+			gadgets[idx].octet0 = hex_str[i];
+			octet_index++;
+		}
+		else if (octet_index == 1) {
+			gadgets[idx].octet1 = hex_str[i];
+			octet_index++;
+		}
+		else {
+			gadgets[idx].octet2 = hex_str[i];
+			octet_index = 0;
+			idx++;
+		}
+	}
+
+	return gadgets;
+}
+
+const char *hex_to_base64(const char *hex_str) 
+{
+	const int hex_str_len = strlen(hex_str);
+	const int gadget_array_len = gadget_arr_len(hex_str_len);
+	encoding_gadget *gadgets = build_gadget_array(hex_str, hex_str_len, gadget_array_len);
+
+
+	free(gadgets);
+	return "blah";
 }
