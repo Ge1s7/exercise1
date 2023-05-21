e9683b8cae78d656b04ea552f4a469e058dc1981
Marco Palma <metatron64@outlook.com>
2023-05-20 20:36:57 -0500

    .

--- driver.c
+++ driver.c
@@ -2,14 +2,14 @@
 #include <assert.h>
 #include "crypto/set1.h"
 
-int main()
+const size_t BUFFER_SIZE = 255;
+
+void base64()
 {
 	const char test_str_1[] = "4d";
 	const char test_str_2[] = "4d61";
 	const char test_str_3[] = "4d616e";
 	const char test_str_4[] = "49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d";
-
-	const size_t BUFFER_SIZE = 255;
 	char result1[BUFFER_SIZE];
 	hex_to_base64(test_str_1, result1, sizeof(result1));
 	assert(strcmp("TQ", result1) == 0);
@@ -25,6 +25,23 @@ int main()
 	char result4[BUFFER_SIZE];
 	hex_to_base64(test_str_4, result4, sizeof(result4));
 	assert(strcmp("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t", result4) == 0);
+}
+
+void fixed_xor()
+{
+	const char str1[] = "1c0111001f010100061a024b53535009181c";
+	const char str2[] = "686974207468652062756c6c277320657965";
+	const char expected_result[] = "746865206b696420646f6e277420706c6179";
+	char result[strlen(str1)];
+
+	strxor(str1, str2, result);
+	assert(strcmp(expected_result, result) == 0);
+}
+
+int main()
+{
+	base64();
+	fixed_xor();
 
 	return 0;
 }
